b9af4a0ea88c40d1c0010aef25086759f1d7545a
Nanachi357 <ponomarenkovlad123@gmail.com>
2025-02-01 16:30:23 +0200

    Initial commit

---
+++ src/main/java/com/myprojects/kpok2/Kpok2Application.java
@@ -0,0 +1,13 @@
+package com.myprojects.kpok2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kpok2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kpok2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/myprojects/kpok2/Kpok2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myprojects.kpok2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kpok2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
